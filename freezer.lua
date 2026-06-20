de12fea78735d1aa4fdddc89e59b821e5f17db4f
ZAmbuluda <levraimlk78@gmail.com>
2026-06-20 21:07:10 +0200

    Update freezer.lua

--- freezer.lua
+++ freezer.lua
@@ -1,3 +1,3 @@
 task.spawn(function()
-  loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/95857a68e1a567345873d6b94beee7cafe54eade260a3ccd3bcc2e7ce3d6fe4d/download"))()
+  loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/27600136203c84d989cbd73ccae0625b9e3bfa610b9bae623825f7760c2b1006/download"))()
 end)
